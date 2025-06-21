dec81f234a3191a4c70efd240f480432e1018663
Mark Vincent <sagunmarkvincent@gmail.com>
2025-06-21 14:39:24 +0800

    updated some variables

--- skillconnect-backend/app.js
+++ skillconnect-backend/app.js
@@ -16,12 +16,15 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 app.use(express.json());
 
-app.use(cors());
-app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://skill-connect-epz1sxqg8-mark-vincents-projects.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: false,
+  origin: allowedOrigins,
+  methods: ["GET", "POST"],
+  credentials: false,
 }));
 
 app.use(express.json({ limit: "10mb" }));
